54023c4f9bde8442e4cbba673824175bf3c110d0
Suraj Kumar <razz912340@gmail.com>
2026-05-31 19:04:32 +0530

    connect frontend to backend api

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,10 +8,9 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
-      // console.log(res.data);
-      setAllHoldings(res.data);
-    });
+    axios.get("https://zerodha-clone-coln.onrender.com/allHoldings").then((res) => {
+  setAllHoldings(res.data);
+});
   }, []);
 
   // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
