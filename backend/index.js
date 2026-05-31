542870bab12a7638176fa440d1f32a3a881df3d1
Suraj Kumar <razz912340@gmail.com>
2026-05-31 18:05:05 +0530

    uncomment addHoldings

--- backend/index.js
+++ backend/index.js
@@ -23,57 +23,58 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.get("/addHoldings", async (req, res) => {
-//   let tempHoldings = [
-//     {
-//       name: "BHARTIARTL",
-//       qty: 2,
-//       avg: 538.05,
-//       price: 541.15,
-//       net: "+0.58%",
-//       day: "+2.99%",
-//     },
-//     {
-//       name: "HDFCBANK",
-//       qty: 2,
-//       avg: 1383.4,
-//       price: 1522.35,
-//       net: "+10.04%",
-//       day: "+0.11%",
-//     },
-//     {
-//       name: "HINDUNILVR",
-//       qty: 1,
-//       avg: 2335.85,
-//       price: 2417.4,
-//       net: "+3.49%",
-//       day: "+0.21%",
-//     },
-//     {
-//       name: "INFY",
-//       qty: 1,
-//       avg: 1350.5,
-//       price: 1555.45,
-//       net: "+15.18%",
-//       day: "-1.60%",
-//       isLoss: true,
-//     },
-//     {
-//       name: "ITC",
-//       qty: 5,
-//       avg: 202.0,
-//       price: 207.9,
-//       net: "+2.92%",
-//       day: "+0.80%",
-//     },
-//     {
-//       name: "KPITTECH",
-//       qty: 5,
-//       avg: 250.3,
-//       price: 266.45,
-//       net: "+6.45%",
-//       day: "+3.54%",
-//     },
+app.get("/addHoldings", async (req, res) => {
+   let tempHoldings = [
+     {
+       name: "BHARTIARTL",
+       qty: 2,
+       avg: 538.05,
+       price: 541.15,
+       net: "+0.58%",
+       day: "+2.99%",
+     },
+          {
+     name: "HDFCBANK",
+     qty: 2,
+     avg: 1383.4,
+     price: 1522.35,
+     net: "+10.04%",
+      day: "+0.11%",
+    },
+   {
+      name: "HINDUNILVR",
+      qty: 1,
+      avg: 2335.85,
+      price: 2417.4,
+      net: "+3.49%",
+      day: "+0.21%",
+    },
+ {
+     name: "INFY",
+     qty: 1,
+     avg: 1350.5,
+     price: 1555.45,
+     net: "+15.18%",
+     day: "-1.60%",
+     isLoss: true,
+    },
+    {
+       name: "ITC",
+       qty: 5,
+       avg: 202.0,
+       price: 207.9,
+       net: "+2.92%",
+       day: "+0.80%",
+    },
+     {
+       name: "KPITTECH",
+      qty: 5,
+       avg: 250.3,
+      price: 266.45,
+      net: "+6.45%",
+      day: "+3.54%",
+    },
+  ];
 //     {
 //       name: "M&M",
 //       qty: 2,
@@ -134,63 +135,57 @@ app.use(bodyParser.json());
 //       net: "+18.08%",
 //       day: "+0.32%",
 //     },
-//   ];
 
-//   tempHoldings.forEach((item) => {
-//     let newHolding = new HoldingsModel({
-//       name: item.name,
-//       qty: item.qty,
-//       avg: item.avg,
-//       price: item.price,
-//       net: item.day,
-//       day: item.day,
-//     });
+ tempHoldings.forEach((item) => {
+    let newHolding = new HoldingsModel({
+       name: item.name,
+       qty: item.qty,
+       avg: item.avg,
+       price: item.price,
+       net: item.day,
+       day: item.day,
+   });
 
-//     newHolding.save();
-//   });
-//   res.send("Done!");
-// });
+     newHolding.save();
+ });
+   res.send("Done!");
+ });
 
-// app.get("/addPositions", async (req, res) => {
-//   let tempPositions = [
-//     {
-//       product: "CNC",
-//       name: "EVEREADY",
-//       qty: 2,
-//       avg: 316.27,
-//       price: 312.35,
-//       net: "+0.58%",
-//       day: "-1.24%",
-//       isLoss: true,
-//     },
-//     {
-//       product: "CNC",
-//       name: "JUBLFOOD",
-//       qty: 1,
-//       avg: 3124.75,
-//       price: 3082.65,
-//       net: "+10.04%",
-//       day: "-1.35%",
-//       isLoss: true,
-//     },
-//   ];
+ app.get("/addPositions", async (req, res) => {
+  let tempPositions = [
+    {
+     net: "+0.58%",
+    day: "-1.24%",
+      isLoss: true,
+    },
+   {
+      product: "CNC",
+      name: "JUBLFOOD",
+      qty: 1,
+      avg: 3124.75,
+      price: 3082.65,
+      net: "+10.04%",
+      day: "-1.35%",
+      isLoss: true,
+     },
+  ];
 
-//   tempPositions.forEach((item) => {
-//     let newPosition = new PositionsModel({
-//       product: item.product,
-//       name: item.name,
-//       qty: item.qty,
-//       avg: item.avg,
-//       price: item.price,
-//       net: item.net,
-//       day: item.day,
-//       isLoss: item.isLoss,
-//     });
+  tempPositions.forEach((item) => {
+    let newPosition = new PositionsModel({
+       product: item.product,
+      name: item.name,
+      qty: item.qty,
+      avg: item.avg,
+      price: item.price,
+      net: item.net,
+      day: item.day,
+       isLoss: item.isLoss,
+     });
 
-//     newPosition.save();
-//   });
-//   res.send("Done!");
-// });
+     newPosition.save();
+  });
+  res.send("Done!");
+ });
 
 
 app.get("/allHoldings", async (req, res) => {
