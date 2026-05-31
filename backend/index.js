c7f3bfb6caf54d5d84523e36216cee36f8b4a0ac
Suraj Kumar <razz912340@gmail.com>
2026-05-31 18:39:05 +0530

    fix positions data

--- backend/index.js
+++ backend/index.js
@@ -92,10 +92,16 @@ app.use(bodyParser.json());
  app.get("/addPositions", async (req, res) => {
   let tempPositions = [
     {
-     net: "+0.58%",
-    day: "-1.24%",
-      isLoss: true,
-    },
+  product: "CNC",
+  name: "EVEREADY",
+  qty: 2,
+  avg: 316.27,
+  price: 312.35,
+  net: "+0.58%",
+  day: "-1.24%",
+  isLoss: true,
+},
+    
    {
       product: "CNC",
       name: "JUBLFOOD",
