32e94b41712fe2234a7b7be76f9d14d72a1fabc1
Suraj Kumar <razz912340@gmail.com>
2026-06-01 11:37:16 +0530

    show orders from database

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,16 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://zerodha-clone-coln.onrender.com/allOrders")
+      .then((res) => {
+        setOrders(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="orders">
-      <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+      <h3>Orders</h3>
 
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
-      </div>
+      {orders.length === 0 ? (
+        <p>You haven't placed any orders today</p>
+      ) : (
+        orders.map((order, index) => (
+          <div key={index}>
+            <p>
+              {order.name} - {order.mode} - Qty: {order.qty}
+            </p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
